1fafd0dc4ad4c8ca35f0399777a01f91b87a2d9e
Albert Urbina <johan50693@gmail.com>
2023-06-13 13:26:38 -0400

    Se crea navigator para navegar entre pantallas

--- App.tsx
+++ App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import { Navigation } from './src/navigattion/Navigation';
 
 const App = () => {
   return (
-    <View>
-      <Text> AppScreenn </Text>
-    </View>
+    <NavigationContainer>
+      <Navigation/>
+    </NavigationContainer>
   );
 };
 
---
+++ src/screens/HomeScreen.tsx
@@ -0,0 +1,12 @@
+import { useNavigation } from '@react-navigation/core'
+import React from 'react';
+import { View, Text, Button } from 'react-native';
+
+export const HomeScreen = () => {
+
+  return (
+      <View>
+        <Text> HomeScreen </Text>
+      </View>
+  );
+};
