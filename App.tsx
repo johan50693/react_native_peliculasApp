3390d9ea5d086c330245ff38ee294586432ee93b
Albert Urbina <johan50693@gmail.com>
2023-06-13 12:18:54 -0400

    Estructura inicial del proyecto

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const App = () => {
+  return (
+    <View>
+      <Text> AppScreenn </Text>
+    </View>
+  );
+};
+
+export default App;
