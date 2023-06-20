5aa5084cd7a64df86540fdcbfd2f34bb01dbae1e
Albert Urbina <johan50693@gmail.com>
2023-06-20 16:50:49 -0400

    Ejercicio para hacer fadeIn

--- App.tsx
+++ App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import { Navigation } from './src/navigattion/Navigation';
+import { FadeScreen } from './src/screens/FadeScreen';
 
 const App = () => {
   return (
     <NavigationContainer>
-      <Navigation/>
+      {/* <Navigation/> */}
+      <FadeScreen />
     </NavigationContainer>
   );
 };
