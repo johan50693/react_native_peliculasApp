366a0cd3a2e73cbe1135277f81c72d312b0813f3
Albert Urbina <johan50693@gmail.com>
2023-06-22 16:50:16 -0400

    Se crea contexto paracomunicar el cambio de colores

--- App.tsx
+++ App.tsx
@@ -1,11 +1,22 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import { Navigation } from './src/navigattion/Navigation';
+import { GradientProvider } from './src/context/GradientContext';
+
+const Appstate = ({children}: any) => {
+  return (
+    <GradientProvider>
+      {children}
+    </GradientProvider>
+  );
+}
 
 const App = () => {
   return (
     <NavigationContainer>
-      <Navigation/>
+      <Appstate>
+        <Navigation/>
+      </Appstate>
     </NavigationContainer>
   );
 };
